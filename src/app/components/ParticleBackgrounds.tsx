e0eaa620edbe835446b26a2a9667c74eb9d1e16b
tech@imcapsule.io <umarsuhail112@gmail.com>
2024-11-21 10:26:09 +0530

    chat fixes

--- src/app/components/ParticleBackgrounds.tsx
+++ src/app/components/ParticleBackgrounds.tsx
@@ -21,6 +21,7 @@ const ParticlesBackground = () => {
   }, []);
 
   const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
   };
 
   const options: ISourceOptions = useMemo(
