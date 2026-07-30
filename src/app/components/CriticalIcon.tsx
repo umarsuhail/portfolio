ae2b2051eaa05bf8441018b224a64d46a37028d8
umarsuhail <umarsuhail112@gmail.com>
2026-07-30 18:04:38 +0400

    the new ch

--- src/app/components/CriticalIcon.tsx
+++ src/app/components/CriticalIcon.tsx
@@ -10,6 +10,7 @@ export type CriticalIconName =
   | "github"
   | "instagram"
   | "linkedin"
+  | "logout"
   | "star"
   | "user"
   | "verified";
@@ -29,6 +30,7 @@ const paths: Partial<Record<CriticalIconName, React.ReactNode>> = {
   download: <path d="M12 3v10m0 0 4-4m-4 4-4-4M5 17v2h14v-2" />,
   eye: <path d="M2.5 12s3.5-6 9.5-6 9.5 6 9.5 6-3.5 6-9.5 6-9.5-6-9.5-6Zm9.5 3a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />,
   file: <path d="M7 3h7l3 3v15H7V3Zm7 0v4h4M9.5 12h5M9.5 16h5" />,
+  logout: <path d="M9 8h6m-3-3v6m-6 2H5a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h4m6 10h4a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2h-4" />,
   star: <path d="m12 3 2.7 5.5 6.1.9-4.4 4.3 1 6.1-5.4-2.9-5.4 2.9 1-6.1-4.4-4.3 6.1-.9L12 3Z" />,
   user: <path d="M12 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8Zm-7 8a7 7 0 0 1 14 0" />,
   verified: <path d="m9.2 12.4 2 2 4-5M12 3l2.2 1.5 2.7.1 1.1 2.5 2.1 1.7-.7 2.6.7 2.6-2.1 1.7-1.1 2.5-2.7.1L12 21l-2.2-1.5-2.7-.1L6 16.9l-2.1-1.7.7-2.6-.7-2.6L6 8.3l1.1-2.5 2.7-.1L12 3Z" />,
